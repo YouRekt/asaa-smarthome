e1f937dcf26e41711f0ac4ff24068e72a721fedc
YouRekt <czarnecki.jurko@gmail.com>
2025-04-10 22:46:15 +0200

    feat: Added logging with simulation time

---
+++ src/main/java/org/asaa/logging/SimTime.java
@@ -0,0 +1,28 @@
+package org.asaa.logging;
+
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.core.pattern.ConverterKeys;
+import org.apache.logging.log4j.core.pattern.LogEventPatternConverter;
+import org.apache.logging.log4j.core.pattern.PatternConverter;
+import org.asaa.environment.Environment;
+
+@Plugin(name = "SimTime", category = PatternConverter.CATEGORY)
+@ConverterKeys({"simTime"})
+public class SimTime extends LogEventPatternConverter {
+
+    protected SimTime(String name, String style) {
+        super(name, style);
+    }
+
+    @PluginFactory
+    public static SimTime newInstance(String[] options) {
+        return new SimTime("simTime", "simTime");
+    }
+
+    @Override
+    public void format(LogEvent event, StringBuilder toAppendTo) {
+        toAppendTo.append(Environment.getSimulationTimeString());
+    }
+}
