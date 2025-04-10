efe02bfe84e21dfc7abacd5b5303bd1ce9c3f4fd
YouRekt <czarnecki.jurko@gmail.com>
2025-04-11 01:36:16 +0200

    feat: Scaffolding for smart appliance agent communication

---
+++ src/main/java/org/asaa/behaviours/appliance/HandleMessageBehaviour.java
@@ -0,0 +1,47 @@
+package org.asaa.behaviours.appliance;
+
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import org.apache.logging.log4j.Logger;
+
+public abstract class HandleMessageBehaviour extends CyclicBehaviour {
+    //TODO: add ApplianceAgent
+    protected final Logger logger;
+
+    public HandleMessageBehaviour(Agent a, Logger logger) {
+        super(a);
+        this.logger = logger;
+    }
+
+    @Override
+    public void action() {
+        final ACLMessage msg = myAgent.receive();
+
+        if (msg != null) {
+            switch (msg.getPerformative()) {
+                case ACLMessage.INFORM -> handleInform(msg);
+                case ACLMessage.REFUSE -> handleRefuse(msg);
+                case ACLMessage.AGREE -> handleAgree(msg);
+                case ACLMessage.FAILURE -> handleFailure(msg);
+                default -> block();
+            }
+        } else {
+            block();
+        }
+    }
+
+    protected abstract void handleInform(ACLMessage msg);
+
+    private void handleAgree(ACLMessage msg) {
+        //TODO: add sensor to subscribed sensor list
+    }
+
+    private void handleRefuse(ACLMessage msg) {
+        logger.warn("{} has refused the subscription", msg.getSender().getLocalName());
+    }
+
+    private void handleFailure(ACLMessage msg) {
+        logger.warn("{} has failed", msg.getSender().getLocalName());
+    }
+}
