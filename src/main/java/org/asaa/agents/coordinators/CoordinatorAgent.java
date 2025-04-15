547acda34ff4f5cacf35f293cd775cef98f08768
Mateusz Małachowski <01180667@pw.edu.pl>
2025-04-15 13:45:35 +0200

    feat: add AgentScanningBehaviour for CoordinatorAgent

--- src/main/java/org/asaa/agents/coordinators/CoordinatorAgent.java
+++ src/main/java/org/asaa/agents/coordinators/CoordinatorAgent.java
@@ -2,26 +2,21 @@ package org.asaa.agents.coordinators;
 
 import jade.core.AID;
 import jade.core.Agent;
-import jade.domain.DFService;
-import jade.domain.FIPAAgentManagement.DFAgentDescription;
-import jade.domain.FIPAAgentManagement.Property;
-import jade.domain.FIPAAgentManagement.ServiceDescription;
-import jade.domain.FIPAException;
+import lombok.Getter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.asaa.behaviours.coordinator.AgentScanningBehaviour;
+import org.asaa.behaviours.coordinator.HandleMessageBehaviour;
 import org.asaa.environment.Area;
-import org.asaa.environment.Environment;
-import org.asaa.exceptions.InvalidServiceSpecification;
 
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
+@Getter
 public final class CoordinatorAgent extends Agent {
     private final Map<Area, Map<String , List<AID>>> physicalAgents = new HashMap<>();
     private Logger logger;
 
-
-    //TODO: Add periodic scanning behaviour for new agents
     @Override
     protected void setup() {
         logger = LogManager.getLogger(getLocalName());
@@ -32,31 +27,12 @@ public final class CoordinatorAgent extends Agent {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        Set<String> areas = Environment.getInstance().getAllAreaNames();
 
-        for (String area : areas) {
-            Map<String, List<AID>> agents = new HashMap<>();
-            final Property property = new Property();
-            property.setName("areaName");
-            property.setValue(area);
+        addBehaviour(new AgentScanningBehaviour(this, 5000));
 
-            final ServiceDescription sd = new ServiceDescription();
-            sd.addProperties(property);
+        addBehaviour(new HandleMessageBehaviour(this) {
 
-            try {
-                final DFAgentDescription dfd = new DFAgentDescription();
-                dfd.addServices(sd);
-                final List<AID> foundAgents = Arrays.stream(DFService.search(this, dfd)).map(DFAgentDescription::getName).toList();
-
-                for (AID agent : foundAgents)
-                    agents.put(agent.getClass().getSimpleName(), foundAgents);
-
-                logger.info("Found {} agents:\n {}", foundAgents.size(), foundAgents);
-                physicalAgents.put(Environment.getInstance().getArea(area), agents);
-            } catch (FIPAException e) {
-                throw new InvalidServiceSpecification(e);
-            }
-        }
+        });
 
     }
 }
---
+++ src/main/java/org/asaa/behaviours/coordinator/AgentScanningBehaviour.java
@@ -0,0 +1,51 @@
+package org.asaa.behaviours.coordinator;
+
+import jade.core.AID;
+import jade.core.behaviours.TickerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.Property;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import org.asaa.agents.coordinators.CoordinatorAgent;
+import org.asaa.environment.Environment;
+import org.asaa.exceptions.InvalidServiceSpecification;
+
+import java.util.*;
+
+public class AgentScanningBehaviour extends TickerBehaviour {
+    private final CoordinatorAgent coordinatorAgent;
+    public AgentScanningBehaviour(CoordinatorAgent coordinatorAgent, long period) {
+        super(coordinatorAgent, period);
+        this.coordinatorAgent = coordinatorAgent;
+    }
+
+    @Override
+    protected void onTick() {
+        Set<String> areas = Environment.getInstance().getAllAreaNames();
+
+        for (String area : areas) {
+            Map<String, List<AID>> agents = new HashMap<>();
+            final Property property = new Property();
+            property.setName("areaName");
+            property.setValue(area);
+
+            final ServiceDescription sd = new ServiceDescription();
+            sd.addProperties(property);
+
+            try {
+                final DFAgentDescription dfd = new DFAgentDescription();
+                dfd.addServices(sd);
+                final List<AID> foundAgents = Arrays.stream(DFService.search(coordinatorAgent, dfd)).map(DFAgentDescription::getName).toList();
+
+                for (AID agent : foundAgents)
+                    agents.put(agent.getClass().getSimpleName(), foundAgents);
+
+                coordinatorAgent.getLogger().info("Found {} agents:\n {}", foundAgents.size(), foundAgents);
+                coordinatorAgent.getPhysicalAgents().put(Environment.getInstance().getArea(area), agents);
+            } catch (FIPAException e) {
+                throw new InvalidServiceSpecification(e);
+            }
+        }
+    }
+}
