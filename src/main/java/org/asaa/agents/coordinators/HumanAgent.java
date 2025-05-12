a6c277390639c43fa59c82890c5f0f216a799ccf
Mateusz Małachowski <01180667@pw.edu.pl>
2025-05-13 00:49:10 +0200

    feat: add HumanAgent sending messages from frontend

--- src/main/java/org/asaa/agents/coordinators/HumanAgent.java
+++ src/main/java/org/asaa/agents/coordinators/HumanAgent.java
@@ -1,17 +1,32 @@
 package org.asaa.agents.coordinators;
 
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.TickerBehaviour;
 import org.asaa.agents.PhysicalAgent;
 import org.asaa.behaviours.human.HandleMessageBehaviour;
+import org.asaa.services.EnvironmentService;
+import org.asaa.services.HumanCommunicationService;
+import org.asaa.util.SpringContext;
 
 public final class HumanAgent extends PhysicalAgent {
+    private final HumanCommunicationService humanCommunicationService = SpringContext.get().getBean(HumanCommunicationService.class);
+
     @Override
     protected void setup() {
         super.setup();
 
         addBehaviour(new HandleMessageBehaviour(this));
-    }
 
-    private void sendFrontendMessage();
+        addBehaviour(new TickerBehaviour(this, 1000) {
+            @Override
+            public void onTick() {
+                if (!humanCommunicationService.getReceivedMessages().isEmpty()) {
+                    logger.info("Sent message");
+                    ((HumanAgent)myAgent).sendMessage(humanCommunicationService.getReceivedMessages().removeFirst());
+                }
+            }
+        });
+    }
 
     @Override
     protected void handleTrigger() {
