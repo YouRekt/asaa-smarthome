21eda4b5a90f1a6512eeed2fdc47309f19536abf
YouRekt <czarnecki.jurko@gmail.com>
2025-06-21 01:34:36 +0200

    refactor: slight modifications to the SmartApplianceAgent's message handler to handle new TaskBehaviours and TaskManagerBehaviour

--- src/main/java/org/asaa/behaviours/appliances/base/MessageHandlerBehaviour.java
+++ src/main/java/org/asaa/behaviours/appliances/base/MessageHandlerBehaviour.java
@@ -3,6 +3,7 @@ package org.asaa.behaviours.appliances.base;
 import jade.core.behaviours.OneShotBehaviour;
 import jade.lang.acl.ACLMessage;
 import org.asaa.agents.base.SmartApplianceAgent;
+import org.asaa.behaviours.appliances.TaskBehaviour;
 import org.asaa.behaviours.base.BaseMessageHandlerBehaviour;
 import org.asaa.util.Util;
 
@@ -39,18 +40,27 @@ public abstract class MessageHandlerBehaviour extends BaseMessageHandlerBehaviou
                 agent.handleToggle(msg.getContent());
                 break;
             case "pause-task":
-                if (agent.getCurrentTask() != null) {
-                    agent.getCurrentTask().pause(false);
+//                if (agent.getCurrentTask() != null) {
+//                    agent.getCurrentTask().pause(false);
+//                }
+                if (agent.getCurrentTaskBehaviour() != null) {
+                    agent.getCurrentTaskBehaviour().pause();
                 }
                 break;
             case "resume-task":
-                if (agent.getCurrentTask() != null) {
-                    agent.getCurrentTask().resume();
+//                if (agent.getCurrentTask() != null) {
+//                    agent.getCurrentTask().resume();
+//                }
+                if (agent.getCurrentTaskBehaviour() != null) {
+                    agent.getCurrentTaskBehaviour().resume();
                 }
                 break;
             case "interrupt-task":
-                if (agent.getCurrentTask() != null) {
-                    agent.getCurrentTask().interrupt(false);
+//                if (agent.getCurrentTask() != null) {
+//                    agent.getCurrentTask().interrupt(false);
+//                }
+                if (agent.getCurrentTaskBehaviour() != null) {
+                    agent.getCurrentTaskBehaviour().interrupt(false, false);
                 }
                 break;
             default:
@@ -62,11 +72,15 @@ public abstract class MessageHandlerBehaviour extends BaseMessageHandlerBehaviou
     protected void handleInform(ACLMessage msg) {
         switch ((msg.getConversationId() == null ? " " : msg.getConversationId())) {
             case "retard":
-                if(agent.getCurrentTask() != null) {
+//                if (agent.getCurrentTask() != null) {
+//                    agent.getLogger().error("Task is not null");
+//                    return;
+//                }
+                if (agent.getCurrentTaskBehaviour() != null) {
                     agent.getLogger().error("Task is not null");
                     return;
                 }
-                agent.addBehaviour(new RelinquishPowerBehaviour(agent,agent.getIdleDraw(),"disable-passive"));
+                agent.addBehaviour(new RelinquishPowerBehaviour(agent, agent.getIdleDraw(), "disable-passive"));
                 break;
             case " ":
                 agent.getLogger().error("CONVERSATION ID WAS NULL !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
@@ -85,12 +99,13 @@ public abstract class MessageHandlerBehaviour extends BaseMessageHandlerBehaviou
                 break;
             case "enable-active":
                 agent.getLogger().info("Coordinator AGREED: {}", msg.getContent());
-                String replyWith = msg.getInReplyTo();
-                Runnable callback = agent.onPowerGrantedCallbacks.remove(replyWith);
-                if (callback != null) {
-                    agent.getLogger().debug("Callback triggered: {}", callback);
-                    callback.run();
-                }
+//                String replyWith = msg.getInReplyTo();
+//                Runnable callback = agent.onPowerGrantedCallbacks.remove(replyWith);
+//                if (callback != null) {
+//                    agent.getLogger().debug("Callback triggered: {}", callback);
+//                    callback.run();
+//                }
+                agent.getCurrentTaskBehaviour().setStatus(TaskBehaviour.Status.powerGranted);
                 break;
             default:
                 break;
@@ -107,12 +122,12 @@ public abstract class MessageHandlerBehaviour extends BaseMessageHandlerBehaviou
             case "enable-active":
                 agent.getLogger().warn("Coordinator REFUSED enable-active:{}", msg.getContent());
                 agent.agentCommunicationController.sendError(agent.getLocalName(), "Active power on refused");
-                String replyWith = msg.getInReplyTo();
-                Runnable callback = agent.onPowerGrantedCallbacks.remove(replyWith);
-                if (callback != null) {
-                    agent.getLogger().warn("Callback cancelled tied with request {}", replyWith);
-                    agent.agentCommunicationController.sendError(agent.getLocalName(), "Callback action was cancelled: request " + replyWith);
-                }
+//                String replyWith = msg.getInReplyTo();
+//                Runnable callback = agent.onPowerGrantedCallbacks.remove(replyWith);
+//                if (callback != null) {
+//                    agent.getLogger().warn("Callback cancelled tied with request {}", replyWith);
+//                    agent.agentCommunicationController.sendError(agent.getLocalName(), "Callback action was cancelled: request " + replyWith);
+//                }
                 break;
             default:
                 break;
@@ -130,12 +145,15 @@ public abstract class MessageHandlerBehaviour extends BaseMessageHandlerBehaviou
                 }
                 agent.setCfpInProgress(true);
                 int canFree = 0, prio = agent.getPriority();
-                if (agent.getCurrentTask() != null && !agent.getCurrentTask().isPaused()) {
-                    if (agent.getCurrentTask().isResumable()) {
+//                if (agent.getCurrentTask() != null && !agent.getCurrentTask().isPaused()) {
+                if (agent.getCurrentTaskBehaviour() != null && !(agent.getCurrentTaskBehaviour().getStatus() == TaskBehaviour.Status.paused)) {
+//                    if (agent.getCurrentTask().isResumable()) {
+                    if (agent.getCurrentTaskBehaviour().isPausable()) {
                         canFree = agent.getActiveDraw();
                         prio = agent.getPriority() % 100;
                         agent.getLogger().warn("Power relief CFP: Currently working and resumable, will pause my current task on accept-proposal");
-                    } else if (agent.getCurrentTask().isInterruptible()) {
+//                    } else if (agent.getCurrentTask().isInterruptible()) {
+                    } else if (agent.getCurrentTaskBehaviour().isInterruptible()) {
                         canFree = agent.getActiveDraw();
                         agent.getLogger().warn("Power relief CFP: Currently working and interruptible, will interrupt my current task on accept-proposal");
                     } else {
@@ -150,7 +168,8 @@ public abstract class MessageHandlerBehaviour extends BaseMessageHandlerBehaviou
                     prio = agent.getPriority() % 100 + 200;
                 }
 
-                agent.getLogger().info("Power relief CFP: {} canFree={}W, prio={}, isWorking={}", agent.getLocalName(), canFree, prio, (agent.getCurrentTask() != null ? "yes" : "no"));
+//                agent.getLogger().info("Power relief CFP: {} canFree={}W, prio={}, isWorking={}", agent.getLocalName(), canFree, prio, (agent.getCurrentTask() != null ? "yes" : "no"));
+                agent.getLogger().info("Power relief CFP: {} canFree={}W, prio={}, isWorking={}", agent.getLocalName(), canFree, prio, (agent.getCurrentTaskBehaviour() != null ? "yes" : "no"));
                 ACLMessage propose = msg.createReply();
                 propose.setPerformative(ACLMessage.PROPOSE);
                 propose.setContent(canFree + "," + prio);
@@ -166,8 +185,10 @@ public abstract class MessageHandlerBehaviour extends BaseMessageHandlerBehaviou
         switch (msg.getConversationId()) {
             case "power-relief":
                 int freed = Integer.parseInt(msg.getContent());
-                if (agent.getCurrentTask() != null && agent.getCurrentTask().isResumable()) {
-                    agent.getCurrentTask().pause(true);
+//                if (agent.getCurrentTask() != null && agent.getCurrentTask().isResumable()) {
+//                    agent.getCurrentTask().pause(true);
+                if (agent.getCurrentTaskBehaviour() != null && agent.getCurrentTaskBehaviour().isPausable()) {
+                    agent.getCurrentTaskBehaviour().pause();
                 } else {
                     agent.addBehaviour(new RelinquishPowerBehaviour(agent, freed, "disable-passive-cfp"));
                 }
