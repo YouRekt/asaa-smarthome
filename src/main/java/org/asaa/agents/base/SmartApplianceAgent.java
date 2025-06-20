7bbe1cb591e3ecc9e6fbc1e727baa2b4bd09fee3
YouRekt <czarnecki.jurko@gmail.com>
2025-06-21 01:34:11 +0200

    refactor: slight modifications to the SmartApplianceAgent to handle new TaskBehaviours and TaskManagerBehaviour

--- src/main/java/org/asaa/agents/base/SmartApplianceAgent.java
+++ src/main/java/org/asaa/agents/base/SmartApplianceAgent.java
@@ -5,24 +5,29 @@ import jade.core.behaviours.Behaviour;
 import jade.lang.acl.ACLMessage;
 import lombok.Getter;
 import lombok.Setter;
+import org.asaa.behaviours.appliances.TaskBehaviour;
+import org.asaa.behaviours.appliances.TaskManagerBehaviour;
 import org.asaa.behaviours.appliances.base.AwaitEnableBehaviour;
 import org.asaa.behaviours.appliances.base.RequestPowerBehaviour;
-import org.asaa.tasks.Task;
+//import org.asaa.tasks.Task;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.PriorityBlockingQueue;
 
 @Getter
 public abstract class SmartApplianceAgent extends PhysicalAgent {
-    public final Map<String, Runnable> onPowerGrantedCallbacks = new ConcurrentHashMap<>();
-
+//    public final Map<String, Runnable> onPowerGrantedCallbacks = new ConcurrentHashMap<>();
     protected final Map<String, List<AID>> subscribedSensors = new HashMap<>();
     protected final List<Runnable> runnables = new ArrayList<>();
     protected final Map<String, Behaviour> behaviours = new HashMap<>();
     protected final long awaitEnablePeriod = 1000;
     private final Queue<ACLMessage> pendingCfpQueue = new LinkedList<>();
+//    @Setter
+//    protected Task currentTask = null;
     @Setter
-    protected Task currentTask = null;
+    protected TaskBehaviour<?> currentTaskBehaviour = null;
+    protected PriorityBlockingQueue<TaskBehaviour<?>> taskBehaviourQueue = new PriorityBlockingQueue<>();
     @Setter
     protected boolean isEnabled = false;
     protected int idleDraw = 0;
@@ -37,6 +42,8 @@ public abstract class SmartApplianceAgent extends PhysicalAgent {
         addBehaviour(new RequestPowerBehaviour(this, idleDraw, priority, "enable-passive", ""));
 
         addBehaviour(new AwaitEnableBehaviour(this, awaitEnablePeriod, runnables, behaviours));
+
+        addBehaviour(new TaskManagerBehaviour(this));
     }
 
     public final void subscribeSensor(AID aid, String sensorType) {
@@ -44,7 +51,8 @@ public abstract class SmartApplianceAgent extends PhysicalAgent {
     }
 
     public void updateStatus() {
-        agentCommunicationController.setAgentStatus(getLocalName(), isEnabled, getCurrentTask() != null && !getCurrentTask().isPaused(), getCurrentTask() == null || getCurrentTask().isInterruptible(), getCurrentTask() == null || getCurrentTask().isResumable(), activeDraw, idleDraw, priority);
+//        agentCommunicationController.setAgentStatus(getLocalName(), isEnabled, getCurrentTask() != null && !getCurrentTask().isPaused(), getCurrentTask() == null || getCurrentTask().isInterruptible(), getCurrentTask() == null || getCurrentTask().isResumable(), activeDraw, idleDraw, priority);
+        agentCommunicationController.setAgentStatus(getLocalName(), isEnabled, currentTaskBehaviour != null && !(currentTaskBehaviour.getStatus() == TaskBehaviour.Status.paused), currentTaskBehaviour == null || currentTaskBehaviour.isInterruptible(), currentTaskBehaviour == null || currentTaskBehaviour.isPausable(), activeDraw, idleDraw, priority);
     }
 
     public void handleToggle(String message) {
