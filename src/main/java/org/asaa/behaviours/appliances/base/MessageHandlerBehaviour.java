9e089797294dbe34f1b0b0a0105fbbb49fcceb80
Mateusz Małachowski <01180667@pw.edu.pl>
2025-06-23 05:35:50 +0200

    fix: WashDishesTask multiple concurrent cfp calls

--- src/main/java/org/asaa/behaviours/appliances/base/MessageHandlerBehaviour.java
+++ src/main/java/org/asaa/behaviours/appliances/base/MessageHandlerBehaviour.java
@@ -56,7 +56,7 @@ public abstract class MessageHandlerBehaviour extends BaseMessageHandlerBehaviou
                 break;
             case "interrupt-task":
                 if (agent.getCurrentTaskBehaviour() != null) {
-                    agent.getCurrentTaskBehaviour().interrupt(false, false);
+                    agent.getCurrentTaskBehaviour().interrupt(true, false);
                 }
                 break;
             default:
